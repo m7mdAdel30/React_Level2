f4a0212052e6c9c6bd8a52b62e33ca54b1dccc27
Mohamed Adel <mfisha30@gmail.com>
2023-10-01 14:33:16 +0300

    useReducder

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,103 @@
 import "./App.css";
 import "./theme.css";
-import { useState } from "react";
+// import { useState } from "react";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      return { ...state, name: action.setValue };
+    case "CHANGE_AGE":
+      return { ...state, age: action.setValue };
+    case "CHANGE_COUNT":
+      return { ...state, startCount: action.setValue };
+    case "CHANGE-MODE":
+      return { ...state, mode: action.setValue };
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [person, setPerson] = useState("Mohamed Adel");
-  const [age, setage] = useState("22");
-  const [count, setcount] = useState(0);
-  const [Theme, setTheme] = useState("Light");
-
-  const changeName = () => {
-    setage("25");
+  const initialData = {
+    name: "Mohamed Adel",
+    age: 22,
+    startCount: 0,
+    mode: "Light",
   };
+  // const [person, setPerson] = useState("Mohamed Adel");
+  // const [age, setage] = useState("22");
+  // const [count, setcount] = useState(0);
+  // const [Theme, setTheme] = useState("Light");
+
+  // const changeName = () => {
+  //   setage("25");
+  // };
+
+  const [Data, dispatch] = useReducer(reducer, initialData);
 
   return (
-    <div className={`App ${Theme}`}>
-      <button className="toggleButton"
-        onClick={() => {
-          setTheme((Theme == 'Light' ? "Dark" : "Light"));
-        }}
-      >
-        Toggle Theme
-      </button>
-
-
-
+    <div className={`App ${Data.mode} `}>
+      <button onClick={() => {
+        dispatch({ type: "CHANGE-MODE", setValue: Data.mode == 'Light' ? 'Dark' : 'Light' });
+      }} className="toggleButton">Toggle Theme</button>
 
       <div className="btn-container">
-  <i className="fa fa-sun-o" aria-hidden="true" />
-  <label className="switch btn-color-mode-switch">
-    <input onChange={() => {setTheme((Theme == 'Light' ? "Dark" : "Light"))}} type="checkbox" name="color_mode" id="color_mode" defaultValue={1} />
-    <label
-      htmlFor="color_mode"
-      data-on="Dark"
-      data-off="Light"
-      className="btn-color-mode-switch-inner"
-    />
-  </label>
-  <i className="fa fa-moon-o" aria-hidden="true" />
-</div>
-
-
-
-
-
-      <div>
-        <button
-          onClick={() => {
-            setTheme("Dark");
-          }}
-          style={{ marginRight: "26px" }}
-        >
-          Dark
-        </button>
-        <button
-          onClick={() => {
-            setTheme("Light");
-          }}
-          style={{ marginRight: "26px" }}
-        >
-          Light
-        </button>
-        <button
-          onClick={() => {
-            setTheme("Gray");
-          }}
-          style={{ marginRight: "26px" }}
-        >
-          Gray
-        </button>
-        <button
-          onClick={() => {
-            setTheme("Pink");
-          }}
-          style={{ marginRight: "26px" }}
-        >
-          Pink
-        </button>
+        <i className="fa fa-sun-o" aria-hidden="true" />
+        <label className="switch btn-color-mode-switch">
+          <input
+          onChange={() => {
+            dispatch({ type: "CHANGE-MODE", setValue: Data.mode == 'Light' ? 'Dark' : 'Light' });
+          }} 
+            type="checkbox"
+            name="color_mode"
+            id="color_mode"
+            defaultValue={1}
+          />
+          <label
+            htmlFor="color_mode"
+            data-on="Dark"
+            data-off="Light"
+            className="btn-color-mode-switch-inner"
+          />
+        </label>
+        <i className="fa fa-moon-o" aria-hidden="true" />
       </div>
 
-      <h2>My name is {person}</h2>
-      <button
-        onClick={() => {
-          setPerson("Fisha");
-        }}
-      >
-        Change name
-      </button>
+      <div>
+        <button onClick={() => {
+          dispatch({ type: "CHANGE-MODE", setValue: 'Dark' });
+        }} style={{ marginRight: "26px" }}>Dark</button>
+        <button onClick={() => {
+          dispatch({ type: "CHANGE-MODE", setValue: 'Light' });
+        }}  style={{ marginRight: "26px" }}>Light</button>
+        <button onClick={() => {
+          dispatch({ type: "CHANGE-MODE", setValue: 'Gray' });
+        }}  style={{ marginRight: "26px" }}>Gray</button>
+        <button onClick={() => {
+          dispatch({ type: "CHANGE-MODE", setValue: 'Pink' });
+        }}  style={{ marginRight: "26px" }}>Pink</button>
+      </div>
+
+      <h2>My name is {Data.name} </h2>
+      <button onClick={() => {
+        dispatch({ type: "CHANGE_NAME", setValue: 'Mido Fisha' });
+      }} >Change name</button>
       <br />
       <br />
 
-      <h2>My Age is {age} Years Old</h2>
-      <button onClick={changeName}>Change Age</button>
+      <h2>My Age is {Data.age} Years Old</h2>
+      <button onClick={() => {
+        dispatch({ type: "CHANGE_AGE", setValue: '27' });
+      }}>Change Age</button>
 
       <br />
       <br />
       <br />
       <br />
-      <button
-        onClick={() => {
-          setcount(count + 1);
-        }}
-      >
-        count is {count}
-      </button>
+      <button onClick={() => {
+        dispatch({ type: "CHANGE_COUNT", setValue: Data.startCount+1 });
+      }} >count is {Data.startCount} </button>
     </div>
   );
 }
